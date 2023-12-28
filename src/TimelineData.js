2a1f67935c5c8dcb2c195cb3b583da43b1e295ee
LYKheang TAING <ltai457@aucklanduni.ac.nz>
2023-12-29 02:38:16 +1300

    second commit

--- src/TimelineData.js
+++ src/TimelineData.js
@@ -1,44 +1,69 @@
 const timelineData = [
     {
       year: 2021,
-      title: "Some Event",
-      content: "Description of what happened in 2021.",
-      skills: ["Skill 1", "Skill 2"]
+      title: "Front of House at MovenPick Ice Cream in New Market, Auckland",
+      content: "At the Ice Cream Cafe in Auckland, my role involved engaging positively with customers, efficiently preparing a range of drinks, maintaining cleanliness, managing busy crowds.",
+      skills: ["customer interaction", "Controlling Crowd", "Postive working Enviroment"]
       
     },
     {
-      year: 2021,
-      title: "Some Event",
-      content: "Description of what happened in 2021.",
-      skills: ["Skill 1", "Skill 2"]
-      
+      title: "Continue my bachelor degree",
+      content: "Bachelor of Science Majoring in Computer Science and Information Technology Management",
+      skills: ["Coding(Java)","Communication","Leadership","Problem Solving","Database:Microsoft SQL Server"]
     },
+    {
+      year: 2020,
+      title: "Started my bachelor degree",
+      content: "Bachelor of Science Majoring in Computer Science and Information Technology Management",
+      skills: ["Coding(Python)","Communication","Leadership","Problem Solving"]
+    },
+    {
+      year: 2020,
+      title: "Finished My Year 13 from UP Education and started my University Of Auckland Journey",
+      content: "Graduated with a good grade and got an offer from University Of Auckland to study Bachelor of Science",
+      skills: ["Calculus", "Physic","Statistic","Chemistry"]
+    },
+   
+    
     {
       year: 2022,
-      title: "University of Auckland Course Coordinator",
-      content: "Description of the coordinator role.",
-      skills: ["Leadership", "Communication"]
+      title: "Volunteer- Auckalnd Uni Khmer Society as a Secretary",
+      content: "At the Auckland Uni Khmer Society, My role involved greeting visitors, guiding them about society activities, and ensuring smooth event operations.",
+      skills: ["Leadership", "Communication","Event Planning","Organise Meeting"]
     },
     
     {
       year: 2023,
       title: "The University Of Auckland",
       content: "Continuing Master Degree in Infomation Technology on July 2023",
-      skills: ["Enrolled in the course INFOSYS 727: Advanced Information Security. Focuses on technical security issues of the systems used in today's information technology applications. Explores the practical issues of identification and authentication, security of operating systems, cryptography, disaster recovery and contingency planning, and discusses the relevant theoretical models. Finally got a certficate of completetion from CISPP. Beside that I enrolled in INFOSYS 750 "]
-    },
-    {
-      year: 2023,
-      title: "UoA Micro Internship,Danone NZ – Auckland, New Zealand",
-      content: "Developed micro service `Display Only` to generate a pdf file based on data of product to reduce waste and manual working processes that are easy to make mistakes.",
-      skills: ["Leadership", "Communication"]
+      skills: ["INFOSYS 727: Security Analysis","COMPSCI 726: Network Protection.","INFOSYS 750: Data Analysis","INFOSYS 730: Telecommunications Management."]
     },
     {
       year: 2023,
       title: "The University Of Auckland",
       content: "Gradauted Bachelor Degree Majoring in Computer Science and Information Technology Management.",
-      skills: ["FullStack Developer(C#, HTML, CSS, JavaScript), Database Implementation(Microsoft SQL Server, MongoDB, Excel), Clean Big Data, Coding Language( Python, Java, JavaScript), Working Independently and Collaborativelty,Advanced in Using Microsoft Word, Excel and create slide in Power Point for presentation "]
+      skills: ["FullStack Development", 
+      "Database Implementation", 
+      "Clean Big Data",
+      "Coding",
+      "Working Independently and Collaborativelty",
+      "Microsoft Word, Excel, PowerPoint"]
       
+    },
+    {
+      year: 2023,
+      title: "UoA Micro Internship,Danone NZ – Auckland, New Zealand Using (Python and JavaScript)" ,
+      content: "Developed micro service `Display Only` to generate a pdf file based on data of product to reduce waste and manual working processes that are easy to make mistakes.",
+      skills: ["Leadership", "Communication","Problem Solving","Coding"]
+    },
+
+    {
+      year: 2023,
+      title: "Backend Developer in the Capstone Project (COMPSCI 399) Using (C#, Microsoft SQL Server, ASP.NET6) ",
+      content: "Goal of this project is to develop an online website for the capstone course which showcases students’ projects to a wide audience, including prospective employers, clients, and other students.",
+      skills: ["Leadership", "Communication","Problem Solving","Coding","Working Independent","Collaborative work"]
     }
+    
     // Add more events as needed
   ];
   export default timelineData;  
